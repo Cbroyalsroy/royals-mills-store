66bdb75a11e795881060945db61180957c4f914b
Cbroyalsroy <cbroyalsroy@gmail.com>
2026-09-02 07:56:10 +0530

    Add files via upload

---
+++ 4bf19ba5.js
@@ -0,0 +1,18 @@
+// Service Worker for GGC Mills PWA
+const CACHE_NAME = 'ggc-mills-v1';
+
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => {
+      return caches.match(e.request);
+    })
+  );
+});
